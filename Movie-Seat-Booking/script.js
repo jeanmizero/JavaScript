13d73eba08d8c0f086b6a598cfe376eefb863926
Jean Mizero <mizero2@gmail.com>
2022-07-03 16:50:51 -0600

    Add javascript

---
+++ Movie-Seat-Booking/script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
+
+populateUI()
+
+let ticketPrice = +movieSelect.value
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
+    const selectedSeatsCount = selectedSeats.length
+
+    count.innerText = selectedSeatsCount
+    total.innerText = selectedSeatsCount * ticketPrice
+}
