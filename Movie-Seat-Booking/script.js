af3703ad9ceb5c06a537a4999a3422b9c26ea5fc
Jean Mizero <mizero2@gmail.com>
2022-07-04 20:58:09 -0600

    Add UI

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -27,3 +27,40 @@ function updateSelectedCount() {
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice
 }
+// Get data from localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
+
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value
+    setMovieData(e.target.selectedIndex, e.target.value)
+    updateSelectedCount()
+})
+
+// Seat click event
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected')
+
+        updateSelectedCount()
+    }
+})
+
+// Initial count and total set
+updateSelectedCount()
